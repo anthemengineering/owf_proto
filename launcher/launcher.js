d31f4c5cd9c1e6524587ccc486941b32a254438e
Joshua Ziggas <jziggas@anthemengineering.com>
2014-05-29 15:16:42 -0400

    chris fixed my crappy code

--- launcher/launcher.js
+++ launcher/launcher.js
@@ -1,6 +1,46 @@
 angular.module('Launcher', []);
 angular.module('Launcher').controller('LauncherCtrl', function($scope) {
+    $scope.apps = [{
+        icon: 'fa-globe',
+        name: 'Geo',
+        id: 'geo'
+    }, {
+        icon: 'fa-warning',
+        name: 'Alerts',
+        id: 'alert'
+    }, {
+        icon: 'fa-cloud',
+        name: 'eXo',
+        id: 'eXo'
+    }, {
+        icon: 'fa-comments-o',
+        name: 'Chat',
+        id: 'chat'
+    }, {
+        icon: 'fa-comment',
+        name: 'Announcements',
+        id: 'announcements'
+    }, {
+        icon: 'fa-search',
+        name: 'Fed Search',
+        id: 'fed_search'
+    }, {
+        icon: 'fa-history',
+        name: 'Activity',
+        id: 'activity'
+    }, {
+        icon: 'fa-space-shuttle',
+        name: 'RFI',
+        id: 'rfi'
+    }, {
+        icon: 'fa-cogs',
+        name: 'ETC',
+        id: ''
+    }];
     $scope.launch = function(name) {
+        if (!name) {
+            return;
+        }
         OWF.Launcher.launch({
             universalName: name + '.anthemengineering.com',
         });
