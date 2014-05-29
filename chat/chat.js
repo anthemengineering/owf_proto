ac963bd37a3b84d0227cdccb7140e32cd999d1ef
Joshua Ziggas <jziggas@anthemengineering.com>
2014-05-29 13:01:29 -0400

    chatbox update

--- chat/chat.js
+++ chat/chat.js
@@ -1,18 +1,21 @@
 angular.module('Chat', []);
 angular.module('Chat').controller('ChatCtrl', function($scope) {
-    $scope.chatLog = [];
+    $scope.chatLog = [{
+        usr: 'Suma',
+        msg: 'Hi there!'
+    }];
     $scope.chat = "";
-    $scope.submitChat = function() {
-        $scope.chatLog.push({
+    $scope.conversation = ['How are you?', 'That\s terrific!', 'I\'m not sure how I feel about that.', 'Nice.', 'You don\'t say?'];
+    $scope.submitChat = function(user) {
+        $scope.chatLog.unshift({
+            usr: user,
             msg: $scope.chat
+        });
+        $scope.chatLog.unshift({
+            usr: 'Suma',
+            msg: $scope.conversation[Math.floor(Math.random() * (4 - 0 + 1)) + 0]
         });
         $scope.chat = "";
     };
 
 });
-
-/*
-
- 
-
-    */
