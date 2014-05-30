7ca90c74b68057ba6c289a6e335b63fde6453d2c
Dan Hooper <dhooper@anthemengineering.com>
2014-05-30 16:00:47 -0400

    Initial code for supporting RFIs and automatic responses

--- rfi/rfi.js
+++ rfi/rfi.js
@@ -1,27 +1,60 @@
 angular.module('RFI', []);
-angular.module('RFI').controller('RFICtrl', function($scope) {
+angular.module('RFI').controller('RFICtrl', function($scope, $timeout) {
     OWF.relayFile = '/owf/js/eventing/rpc_relay.uncompressed.html';
 
+    function getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+    truncateDecimals = function(number, digits) {
+        var multiplier = Math.pow(10, digits),
+            adjustedNum = number * multiplier,
+            truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
+
+        return truncatedNum / multiplier;
+    };
+
+    $scope.possibleResponses = [
+        "Isolate the patient.",
+        "There is nothing to worry about.",
+    ];
+
     $scope.rfis = [];
-    $scope.rfis.unshift({
-        rfiName: 'Test',
-        agency: 'Agency',
-        status: 'Awaiting Response'
-    });
     $scope.onRFIClick = function(rfi) {
         OWF.Launcher.launch({
-            universalName: 'rfidetail.anthemengineering.com',
-        });
+            universalName: 'rfidetail.anthemengineering.com'
+            },
+            function() {
+                OWF.Eventing.publish('RFIDetailView', rfi);
+            }
+        );
     };
     $scope.onRFISubmitReceive = function(sender, rfi) {
-        console.log('received rfi ' + JSON.stringify(rfi));
-        $scope.rfis.unshift(rfi);
+        if(rfi.$$hashKey) {
+            for( i = 0; i < $scope.rfis.length; i++ ) {
+                if($scope.rfis[i].$$hashKey === rfi.$$hashKey) {
+                    $scope.rfis[i] = rfi;
+                }
+            }
+        } else {
+            $scope.rfis.unshift(rfi);
+        }
         $scope.$apply();
-        console.log('rfis ' + JSON.stringify($scope.rfis));
     };
+    $scope.onRFITimeout = function() {
+        for( i = 0; i < $scope.rfis.length; i++ ) {
+            var currRfi = $scope.rfis[i];
+            if(currRfi.status === 'Awaiting Response') {
+                currRfi.status = 'Responded';
+                currRfi.response = $scope.possibleResponses[truncateDecimals(getRandomArbitrary(0, $scope.possibleResponses.length), 0)];
+                OWF.Eventing.publish('RFIUpdate', currRfi);
+            }
+        }
+        $scope.$apply();
+        $timeout($scope.onRFITimeout, 10000);
+    }
     OWF.ready(function() {
         OWF.Eventing.subscribe('RFISubmit', $scope.onRFISubmitReceive);
     });
-
+    $timeout($scope.onRFITimeout, 10000);
 });
 
--- rfi/rfidetail.js
+++ rfi/rfidetail.js
@@ -1,16 +1,31 @@
 angular.module('RFIDetail', []);
 angular.module('RFIDetail').controller('RFIDetailCtrl', function($scope) {
     OWF.relayFile = '/owf/js/eventing/rpc_relay.uncompressed.html';
+    $scope.rfi = {status: "Awaiting Response"};
 
     $scope.submit = function() {
-        rfi = {
-            rfiName: $scope.rfiName,
-            agency: $scope.agency,
-            status: "Awaiting Response"
-        }
-        console.log('publishing ' + JSON.stringify(rfi));
-        OWF.Eventing.publish('RFISubmit', rfi);
+        OWF.Eventing.publish('RFISubmit', $scope.rfi);
+        $scope.rfi = {status: "Awaiting Response"};
+        $scope.apply();
     };
+    $scope.onRFIDetailView = function(sender, rfi) {
+        if(!rfi) {
+            $scope.rfi = {status: "Awaiting Response"};
+        } else {
+            $scope.rfi = rfi;
+        }
+        $scope.$apply();
+    };
+    $scope.onRFIUpdate = function(sender, rfi) {
+        if($scope.rfi.$$hashKey === rfi.$$hashKey) {
+            $scope.rfi = rfi;
+            $scope.$apply();
+        }
+    };
+    OWF.ready(function() {
+        OWF.Eventing.subscribe('RFIDetailView', $scope.onRFIDetailView);
+        OWF.Eventing.subscribe('RFIUpdate', $scope.onRFIUpdate);
+    });
 });
 
 
